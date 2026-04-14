9f327fb89eaeac2b906134f8dd8bd231a9920d1c
VIJETA VIJAYKUMAR NAIK <vijetaviju18@gmail.com>
2026-04-14 23:39:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ INDEX_FILE = BASE_DIR / "index.html"
 app = Flask(__name__, static_folder=str(BASE_DIR), static_url_path="")
 CORS(app)
 
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 @app.route("/")
 def home():
     return send_file(INDEX_FILE)
